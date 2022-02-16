a28c60f92c1cf47ec75872aa1aed29d9e4d856a1
Jonathon Carter <30672425+jnoc@users.noreply.github.com>
2022-02-16 02:11:46 +0000

    Update orchestra.py

--- data-host/testbed/orchestra.py
+++ data-host/testbed/orchestra.py
@@ -1,5 +1,6 @@
 ## This is to be run fron proj-data (the client and data collection machine)
 import subprocess
+import os
 
 # commands
 shutOffNodes = "pssh -i -h shutoff-hosts.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'hostname; sleep 1; init 0'"
@@ -7,18 +8,24 @@ telegrafStart = "pssh -i -h all-nodes.txt -A -l root -x '-tt -q -o StrictHostKey
 telegrafStop = "pssh -i -h all-nodes.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' systemctl stop telegraf"
 nodesOnline = "pssh -i -h all-nodes.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' hostname"
 
+# fio commands
+
+
 # keep track
 commands = [shutOffNodes, telegrafStart, telegrafStop, nodesOnline]
 
 # main program start
 def main():
     set = ["1", "2", "3", "4", "Q", "q"]
-    print("[!] Files in use: shutoff-hosts.txt, all-nodes.txt")
+    print("\n[!] Files in use: shutoff-hosts.txt, all-nodes.txt")
     print('''
     1 = Shut off nodes (shutoff-hosts.txt is used here)
     2 = Start telegraf across the cluster
     3 = Stop telegraf across the cluster
     4 = Check the amount of nodes online across the cluster
+
+    5 = FIO stress test ###################################
+
     Q = Exit program
     ''')
     while True:
@@ -26,6 +33,7 @@ def main():
             option = input("[Enter] an option value: ")
             if str(option) in set:
                 if str(option) == "Q" or str(option) == "q":
+                    print()
                     break
                 else:
                     handle(option)
