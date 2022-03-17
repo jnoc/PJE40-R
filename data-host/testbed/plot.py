cf7becfe314994060f3c0048d670b31ccc5b50f8
Jonathon Carter <30672425+jnoc@users.noreply.github.com>
2022-03-17 15:21:29 +0000

    glusterFS setup

--- data-host/testbed/plot.py
+++ data-host/testbed/plot.py
@@ -65,17 +65,17 @@ commands = [
 #-l, --latency-iops-2d-qd
 #                        Generates a 2D barchart of IOPs and latency for all
 #                        queue depths given a particular numjobs value.
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title 'MooseFS IOPS/Latency per amount of queue depths ({3} reads)' -r {4}read -l --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title 'MooseFS IOPS/Latency per amount of queue depths ({3} writes)' -r {4}write -l --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title '{2} IOPS/Latency per amount of queue depths ({3} reads)' -r {4}read -l --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title '{2} IOPS/Latency per amount of queue depths ({3} writes)' -r {4}write -l --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
 #-L, --iodepth-numjobs-3d
 #                        Generates a 3D-chart with iodepth and numjobs on x/y
 #                        axis and iops or latency on the z-axis.
 
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title 'MooseFS IOPS per users and queue depths ({3} reads)' -r {4}read -L -t iops -f read --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title 'MooseFS IOPS per users and queue depths ({3} writes)' -r {4}write -L -t iops -f write --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title '{2} IOPS per users and queue depths ({3} reads)' -r {4}read -L -t iops -f read --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title '{2} IOPS per users and queue depths ({3} writes)' -r {4}write -L -t iops -f write --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
 
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title 'MooseFS Latency per users and queue depths ({3} reads)' -r {4}read -L -t lat -f read --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title 'MooseFS Latency per users and queue depths ({3} writes)' -r {4}write -L -t lat -f write --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title '{2} Latency per users and queue depths ({3} reads)' -r {4}read -L -t lat -f read --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title '{2} Latency per users and queue depths ({3} writes)' -r {4}write -L -t lat -f write --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
 
 
 #"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/seqread/{1}/4k/ --title 'MooseFS IOPS per users and queue depths (reads)' -r read -L -t iops -f read --disable-fio-version -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
@@ -88,11 +88,11 @@ commands = [
 #                        recorded by FIO.
 
 # latency over 60s of number of jobs
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title 'MooseFS Latency against number of jobs ({3} reads)' -g -r {4}read -f read -t lat -n 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title 'MooseFS Latency against number of jobs ({3} writes)' -g -r {4}write -f write -t lat -n 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title '{2} Latency against number of jobs ({3} reads)' -g -r {4}read -f read -t lat -n 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title '{2} Latency against number of jobs ({3} writes)' -g -r {4}write -f write -t lat -n 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
 
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title 'MooseFS Latency against queue depths ({3} reads)' -g -r {4}read -f read -t lat -d 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
-"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title 'MooseFS Latency against queue depths ({3} writes)' -g -r {4}write -f write -t lat -d 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue)
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}read/{1}/4k/ --title '{2} Latency against queue depths ({3} reads)' -g -r {4}read -f read -t lat -d 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue),
+"fio-plot -i ../../fio-bench/fio-bench-*-{0}-{1}/{0}write/{1}/4k/ --title '{2} Latency against queue depths ({3} writes)' -g -r {4}write -f write -t lat -d 1 2 4 8 32 64 --disable-fio-version --enable-markers -s 'UP896692'".format(typePath, pathname, name, typeResult, rValue)
 ]
 
 ##### Compare command
