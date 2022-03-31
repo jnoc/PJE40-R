f395ae102ae02807a6190c0a602e5b0b0673f72c
Jonathon Carter <30672425+jnoc@users.noreply.github.com>
2022-03-31 11:12:31 +0100

    update 31/03

--- data-host/testbed/module.py
+++ data-host/testbed/module.py
@@ -4,6 +4,7 @@ import alive_progress as ap
 import re
 from statistics import mean
 from knockknock import discord_sender
+from datetime import datetime
 
 from orchestra import export
 
@@ -34,7 +35,7 @@ def module():
         file = "tf"
 
 
-    for i in range(6):
+    for i in range(12):
         try:
             print("Mode == " + file)
             # random and sequential                                                                                                                 ### need to change back to 100M for LOW
@@ -47,17 +48,17 @@ def module():
             lowSRW = ["low-seq-rw-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=low-seq-rw-{0} --filename={0}/test --bs=128k --size=100M --readwrite=rw --ramp_time=4 | tee -a fio-{2}-test-low-seq-rw-{1}.txt".format(mp, file, name)]
 
             ### Added if needed!
-            highRR = ["high-rand-read-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-read-{0} --filename={0}/test --bs=4k --size=10M --readwrite=randread --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-rand-read-{1}.txt".format(mp, file, name)]
-            highRW = ["high-rand-write-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-write-{0} --filename={0}/test --bs=4k --size=10M --readwrite=randwrite --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-rand-write-{1}.txt".format(mp, file, name)]
-            highRRW = ["high-rand-rw-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-rw-{0} --filename={0}/test --bs=4k --size=10M --readwrite=randrw --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-rand-rw-{1}.txt".format(mp, file, name)]
+            highRR = ["high-rand-read-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-read-{0} --filename={0}/test --bs=128k --size=10M --readwrite=randread --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-rand-read-{1}.txt".format(mp, file, name)]
+            highRW = ["high-rand-write-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-write-{0} --filename={0}/test --bs=128k --size=10M --readwrite=randwrite --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-rand-write-{1}.txt".format(mp, file, name)]
+            highRRW = ["high-rand-rw-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-rand-rw-{0} --filename={0}/test --bs=128k --size=10M --readwrite=randrw --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-rand-rw-{1}.txt".format(mp, file, name)]
 
-            highSR = ["high-seq-read-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-read-{0} --filename={0}/test --bs=4k --size=10M --readwrite=read --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-seq-read-{1}.txt".format(mp, file, name)]
-            highSW = ["high-seq-write-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-write-{0} --filename={0}/test --bs=4k --size=10M --readwrite=write --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-seq-write-{1}.txt".format(mp, file, name)]
-            highSRW = ["high-seq-rw-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-rw-{0} --filename={0}/test --bs=4k --size=10M --readwrite=rw --ramp_time=4 --numjobs=8 | tee -a fio-{2}-test-high-seq-rw-{1}.txt".format(mp, file, name)]
+            highSR = ["high-seq-read-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-read-{0} --filename={0}/test --bs=128k --size=10M --readwrite=read --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-seq-read-{1}.txt".format(mp, file, name)]
+            highSW = ["high-seq-write-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-write-{0} --filename={0}/test --bs=128k --size=10M --readwrite=write --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-seq-write-{1}.txt".format(mp, file, name)]
+            highSRW = ["high-seq-rw-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=high-seq-rw-{0} --filename={0}/test --bs=128k --size=10M --readwrite=rw --ramp_time=4 --numjobs=10 | tee -a fio-{2}-test-high-seq-rw-{1}.txt".format(mp, file, name)]
 
             #allLow = [lowRW, lowRR, lowRRW, lowSW, lowSR, lowSRW]
             # rand write, then rand read and finally rand readwrite
-            commands = [lowRW, lowRR, lowRRW, lowSW, lowSR, lowSRW]
+            commands = [lowRW, lowRR, lowRRW, lowSW, lowSR, lowSRW, highRW, highRR, highRRW, highSR, highSW, highSRW]
             print("\n[Start] {}".format(commands[i][0]))
             func(i, commands, mp, name)
         except Exception as e:
@@ -66,22 +67,25 @@ def module():
             return "\n\t\t`Something went wrong with the command!`"
     print()
     output = "```"
-    output+= "\nMountpoint = {0}, Mode = {1}".format(mp, file)
+    output+= "\nMountpoint = {0}, Mode = {1}, Time = Msec, Output Mode = csv".format(mp, file)
+    output+= "\ntestType,min,avg,max"
     for i in range(len(commands)):
-        file = open("fio-{0}-test-{1}.txt".format(name, commands[i][0], 'r'))
-        match = re.findall("-(\d*)msec", file.read(), re.DOTALL)
+        results = open("fio-{0}-test-{1}.txt".format(name, commands[i][0]), 'r')
+        match = re.findall("-(\d*)msec", results.read(), re.DOTALL)
         match = list(map(float, match))
-        output += "\n{0}:\tmin/avg/max {1}msec/{2:.1f}msec/{3}msec".format(commands[i][0], min(match), mean(match), max(match))
+        output += "\n{0},{1},{2:.1f},{3}".format(commands[i][0], min(match), mean(match), max(match))
         print("{0}:\tmin/avg/max {1}msec/{2:.1f}msec/{3}msec".format(commands[i][0], min(match), mean(match), max(match)))
     output += "```"
-    out = sp.Popen("echo '{0}' > fio-{1}-test-{2}.txt".format(output, name, file), shell=True, stdout=sp.PIPE)
+    out = sp.Popen("echo '{0}' > fio-{1}-test-overview-{2}.csv".format(output, name, file), shell=True, stdout=sp.PIPE)
     out.wait()
     return output
     #return "\n\t\t`Script finished`"
 
     
 def func(index, commands, mp, name):
-    mkfile = sp.Popen("echo '' > fio-{0}-test-{1}.txt".format(name,commands[index][0]), shell=True, stdout=sp.PIPE)
+    now = datetime.now()
+    date = now.strftime("%d-%m-%Y--%H-%M-%S")
+    mkfile = sp.Popen("echo 'Started: {2}\n' > fio-{0}-test-{1}.txt".format(name,commands[index][0],date), shell=True, stdout=sp.PIPE)
     mkfile.wait()
     if Tfd == True:
             print("-> Using shutoff-hosts.txt to turn off host(s) mid task.")
@@ -100,7 +104,7 @@ def func(index, commands, mp, name):
                     command = "pssh -i -h shutoff-hosts.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'hostname; sleep {}; init 0'".format(specTime)
                     sp.Popen("{0}{1}".format(sshPass,command), shell=True, stdout=sp.PIPE)
                     print("-> Shutting off node(s)")
-            fioRun = sp.Popen(commands[index][1], shell=True, stdout=sp.PIPE)
+            fioRun = sp.Popen("{0}".format(commands[index][1]), shell=True, stdout=sp.PIPE)
             fioRun.wait()
             fileCleanup = sp.Popen("rm {}/test".format(mp), stdout=sp.PIPE, shell=True)
             fileCleanup.wait()
