9bd268827520671a2d8c18ce4301202ea22b814a
Jonathon Carter <30672425+jnoc@users.noreply.github.com>
2022-04-08 20:32:25 +0100

    update

--- data-host/testbed/module.py
+++ data-host/testbed/module.py
@@ -16,14 +16,14 @@ webhook_url = "https://discord.com/api/webhooks/955584973103042600/bVoLZJewwADPV
 
 def module():
 
-    
+    num_lines = sum(1 for line in open('shutoff-hosts.txt'))
 
-    global mp, name, Tfd, Tnf, Tf, file, sshPass, tnfTimeList
+    global mp, name, Tfd, Tnf, Tf, file, sshPass, tnfTimeList, caughtErrors
     print("Inside module")
 
     sshPass = "sshpass -f sshpass "
     outputArray = export()
-        
+    caughtErrors = []    
     mp = outputArray[0]
     modeType = outputArray[1]
     tnfTimeList = outputArray[2]
@@ -45,7 +45,7 @@ def module():
             run = sp.Popen("ls -la {}".format(mp), shell=True, stdout=sp.PIPE)
             out = run.communicate()[0].decode("utf-8")
             match = re.findall('error|Error', str(out))
-            # try catch errors 
+            # try catch some linux errors 
             if len(match) != 0:
                 #print(out)
                 out = "```" + out + "```"
@@ -60,7 +60,6 @@ def module():
                 out+= "\n`Cluster not mounted, failed on iteration {} of module.py`".format(i+1)
                 return out
 
-            #print("Mode == " + file)
             # random and sequential                                                                                                                 ### need to change back to 100M for LOW
             lowRR =  ["low-rand-read-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=low-rand-read-{0} --filename={0}/test --bs=128k --size=100M --readwrite=randread --ramp_time=4 | tee -a fio-{2}-test-low-rand-read-{1}.txt".format(mp, file, name)]
             lowRW = ["low-rand-write-{0}".format(file), "fio --randrepeat=1 --ioengine=libaio --direct=1 --name=low-rand-write-{0} --filename={0}/test --bs=128k --size=100M --readwrite=randwrite --ramp_time=4 | tee -a fio-{2}-test-low-rand-write-{1}.txt".format(mp, file, name)]
@@ -90,17 +89,21 @@ def module():
             return "\n\t\t`Something went wrong with the command!`"
     print()
     output = "```"
-    output+= "\nMountpoint = {0}, Mode = {1}, Time = Msec, Output Mode = csv".format(mp, file)
+    output+= "\nMountpoint = {0}, Mode = {1}, Failures = {2}, Time = Msec, Output Mode = csv".format(mp, file, num_lines)
     output+= "\ntestType,min,avg,max"
     for i in range(len(commands)):
         results = open("fio-{0}-test-{1}.txt".format(name, commands[i][0]), 'r')
         match = re.findall("-(\d*)msec", results.read(), re.DOTALL)
-        match = list(map(float, match))
-        output += "\n{0},{1},{2:.1f},{3}".format(commands[i][0], min(match), mean(match), max(match))
-        print("{0}:\tmin/avg/max {1}msec/{2:.1f}msec/{3}msec".format(commands[i][0], min(match), mean(match), max(match)))
+        if len(match) != 0:
+            match = list(map(float, match))
+            output += "\n{0},{1},{2:.1f},{3}".format(commands[i][0], min(match), mean(match), max(match))
+            print("{0}:\tmin/avg/max {1}msec/{2:.1f}msec/{3}msec".format(commands[i][0], min(match), mean(match), max(match)))
+    output += "\nCaught Errors: {}".format(caughtErrors)
     output += "```"
     out = sp.Popen("echo '{0}' > fio-{1}-test-overview-{2}.csv".format(output, name, file), shell=True, stdout=sp.PIPE)
     out.wait()
+    # tunr nodes back online at the end of the testing
+    turnOnline()
     return output
     #return "\n\t\t`Script finished`"
 
@@ -130,7 +133,16 @@ def func(index, commands, mp, name):
                 command = "pssh -i -h shutoff-hosts.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'hostname; sleep {}; init 0'".format(specTime)
                 sp.Popen("{0}{1}".format(sshPass,command), shell=True, stdout=sp.PIPE)
                 print("-> Shutting off node(s)")
-            fioRun = sp.Popen("{0}".format(commands[index][1]), shell=True, stdout=sp.PIPE)
+            fioRun = sp.Popen("{0}".format(commands[index][1]), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+            out = fioRun.communicate()[1].decode("utf-8")
+            print(out)
+            ## Endpoint is disconnected mid test or catch other fio error
+            match = re.findall('error|error=|endpoint', str(out))
+            if len(match) != 0:
+                cE = "Iter:{0},Run:{1}".format(commands[index][0],j+1)
+                caughtErrors.append(cE)
+                mkfile = sp.Popen("echo '{0}, Error: {1}' | tee -a fio-errors.txt".format(cE, match), shell=True, stdout=sp.PIPE)
+                mkfile.wait()
             fioRun.wait()
             fileCleanup = sp.Popen("rm {}/test".format(mp), stdout=sp.PIPE, shell=True)
             fileCleanup.wait()
@@ -148,6 +160,8 @@ def turnOnline():
     proc = sp.Popen("{0}{1}".format(sshPass, nodesOnline), stdout=sp.PIPE, shell=True)
     out = proc.communicate()
     match = re.findall('\[FAILURE\]\W(node-\d\d|master-\d\d)', str(out))
+    # pssh bug error catch https://bugzilla.redhat.com/show_bug.cgi?id=1757957
+    matchErr = re.findall('Error|error', str(out))
     if len(match) != 0:
         ## catch any nodes that do not turn on... exit module.py if fails
         if count != 0:
@@ -181,6 +195,10 @@ def turnOnline():
         count+= 1
         print("[Sleep][---] 120s to allow cluster catchup...")
         time.sleep(120)
+        turnOnline()
+    elif len(matchErr) != 0:
+        print("Error caught outside")
+        count+= 1
         turnOnline()
     else:
         print("-> Hosts are back online...")
--- data-host/testbed/test.py
+++ data-host/testbed/test.py
@@ -11,6 +11,7 @@ import subprocess as sp
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+mp = config.get('default','mountpoint')
 sshPass = "sshpass -f sshpass "
 
 def main():
@@ -28,7 +29,7 @@ def main():
     specTime = ((tnfTimeList[0] / 4) / 1000) # avg Tnf / 4 
     print("{} <---- Tnf/4 time".format(specTime))
  """
-    nodesOnline = "pssh -i -h all-nodes.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'echo online'"
+    """ nodesOnline = "pssh -i -h all-nodes.txt -A -l root -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'echo online'"
     proc = sp.Popen("{0}{1}".format(sshPass, nodesOnline), stdout=sp.PIPE, shell=True)
     out = proc.communicate()
     array = re.findall('\[FAILURE\]\W(node-\d\d|master-\d\d)', str(out))
@@ -54,6 +55,7 @@ def main():
                 matchProj = "proj-{}".format(node)
             command = "pssh -i -H {1} -A -l vm1 -x '-tt -q -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no' 'VBoxManage startvm {0} --type headless'".format(turnOn, matchProj)
             print("turning on {0} hosted on {1}".format(turnOn, matchProj))
+            run = sp.Popen("{0}{1}".format("sshpass -p localhost1 ", command), shell=True, stdout=sp.PIPE)
             run.wait()
     else:
         print("-> {} host(s) are offline".format(len(array)))
@@ -64,8 +66,17 @@ def main():
     proc = sp.Popen("{0}{1}".format(sshPass, nodesOnline), stdout=sp.PIPE, shell=True)
     out = proc.communicate()
     array = re.findall('\[FAILURE\]\W(node-\d\d|master-\d\d)', str(out))
-    print("{} are offline".format(array))
-        
+    print("{} are offline".format(array)) """
+    
+    run = sp.Popen("df -h".format(mp), shell=True, stdout=sp.PIPE)
+    out = run.communicate()[0].decode("utf-8")
+    match = re.findall('{}'.format(mp), str(out))
+    print(match)
+    if len(match) == 0:
+        print(out)
+        out+= "\nCluster not mounted"
+        print(out)
+        return out
 
 if __name__ == "__main__":
     main()
